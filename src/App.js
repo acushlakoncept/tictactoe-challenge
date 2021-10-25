c2642734998cfb425292663d03821c982ee440f7
Uduak Essien <acushla4real@gmail.com>
2021-10-25 14:28:52 +0100

    Add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-     <h1>Hello World!</h1>
+     <h1 class="text-success">Hello World!</h1>
+     <Button>Hello There</Button>
     </div>
   );
 }
