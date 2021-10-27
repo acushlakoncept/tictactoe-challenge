3d9dee185d5c22784979014d4f9ac8ec9a4906f4
Uduak Essien <acushla4real@gmail.com>
2021-10-27 12:22:37 +0100

    Add getWinner

---
+++ src/utils/helper.js
@@ -0,0 +1,35 @@
+const getWinner = (squares) => {
+    
+    let winningCombos = {
+        horizontal: [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8] 
+        ],
+        vertical: [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ],
+        diagonal: [
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+    }
+
+    let winnerPiece = null
+    for(let combo in winningCombos){
+        // eslint-disable-next-line no-loop-func
+        winningCombos[combo].forEach( pattern => {
+            const [a,b,c] = pattern
+            console.log('p', squares[a], squares[b], squares[c])
+           if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+               winnerPiece = squares[a]
+           }
+        })
+    }
+
+    return winnerPiece;
+}
+
+export default getWinner;
