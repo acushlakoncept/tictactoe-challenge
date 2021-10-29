3006b58bdff66ff21198840773aaf1e00cfa8ab5
Uduak Essien <acushla4real@gmail.com>
2021-10-29 14:18:12 +0100

    Add handleInputChange and form Submit button for players

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Form } from 'react-bootstrap';
 import GameBoard from './component/GameBoard';
 import getWinner from './utils/helper';
 
@@ -8,11 +8,18 @@ function App() {
   const [cells, setCells] = useState(Array(9).fill(null));
   const [winner, setWinner] = useState(null);
   const [turn, setTurn] = useState('X')
+  const [formData, setFormData] = useState({})
 
   const handleRestartGame = () => {
     setWinner(null)
     setCells(Array(9).fill(null))
     setTurn('X')
+  }
+
+  const startNewPlayers = () => {
+    handleRestartGame()
+    document.getElementById('gameSection').classList.toggle('d-none')
+    document.getElementById('playerForm').classList.toggle('d-none')
   }
 
   const handleClick = (i) => {
@@ -35,20 +42,49 @@ function App() {
     setWinner(getWinner(squares))
   };
 
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    let data = {}
+    data[name] = value;
+    setFormData({...formData, ...data})
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    document.getElementById('gameSection').classList.toggle('d-none')
+    document.getElementById('playerForm').classList.toggle('d-none')
+  }
+
   return (
     <Container className="d-flex flex-column align-items-center">
-    
       <h1 className="text-center">Welcome to Tic Tac Toe Game</h1>
       <hr />
-      <h3>{winner ? `Winner: ${winner}` : !cells.includes(null) ? 'Game is a tie' : `Next Player: ${turn}`}</h3>
+      <Form id="playerForm" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="player1">
+        <Form.Control name="player1" type="text" placeholder="Enter Player 1 Name" onChange={handleInputChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="player2">
+        <Form.Control name="player2" type="text" placeholder="Enter Player 2 Name" onChange={handleInputChange} required />
+      </Form.Group>
+        <Button type="submit" className="mb-2">Submit</Button>
+      </Form>
+
+     <div id="gameSection" className="d-none">
+      <h3>{winner ? `Winner: ${winner === 'X' ? formData['player1'] : formData['player2']}` : !cells.includes(null) ? 
+      'Game is a tie' : `Next Player: ${turn === 'X' ? formData['player1'] : formData['player2']}`}</h3>
       <GameBoard squares={cells} handleClick={handleClick} />
       {
         (winner || !cells.includes(null)) && (
           <>
             <Button className="btn-success mt-2" onClick={()=>handleRestartGame()}>Play Again</Button>
+            <Button className="btn-success mt-2" onClick={()=>startNewPlayers()}>Start with New Players</Button>
           </>
         )
       }
+      </div>
 
     </Container>
   );
