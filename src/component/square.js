68944d4cedcfab35d4cf2ab2d3b743092ac70511
Uduak Essien <acushla4real@gmail.com>
2021-10-29 17:55:14 +0100

    Add proptype for square.js

--- src/component/square.js
+++ src/component/square.js
@@ -1,9 +1,15 @@
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 const Square = ({ value, handleClick }) => (
   <Button className={`square btn-light ${value}`} onClick={handleClick}>
     {value}
   </Button>
 );
+
+Square.propTypes = {
+  value: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Square;
